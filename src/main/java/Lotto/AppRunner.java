94f1984e4888e04eb974cb60a04945b07a314a54
Karcz Przemysław <pkarc@PGI.LOCAL>
2022-07-29 13:26:30 +0200

    general remarks

--- src/main/java/Lotto/AppRunner.java
+++ src/main/java/Lotto/AppRunner.java
@@ -5,7 +5,13 @@ public class AppRunner {
     public static void main(String[] args) {
 
         Numbers numbers = new Numbers();
-        numbers.method();
+        //numbers.method();
+
+        Betting betting = new Betting();
+        betting.bettingNumber();
+
+        RandomNumbers randomNumbers = new RandomNumbers();
+        //randomNumbers.generatesRandomNumbers();
 
 
     }
---
+++ src/main/java/Lotto/DoCwiczen.java
@@ -0,0 +1,43 @@
+package Lotto;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Random;
+
+public class DoCwiczen {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000; i++) {
+
+            getRandomNumberUsingNextInt(1, 49);
+
+
+        }
+
+    }
+
+    public static int getRandomNumberUsingNextInt(int min, int max) {
+        Random random = new Random();
+
+        System.out.println();
+
+        for (int i = 0; i < 6; i++) {
+
+            //int nextInt = random.nextInt(max - min) + min;
+            int nextInt = random.nextInt(50 - 1) + 1;
+
+            if (nextInt == 0){
+
+                System.out.println("0!");
+                System.exit(0);
+
+            }
+
+            System.out.print(nextInt + "|");
+
+        }
+        return random.nextInt(max - min) + min;
+    }
+
+}
