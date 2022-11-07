4f071a378248cc6ea1febfe3ffc2196de90520d3
Karcz Przemysław <pkarc@PGI.LOCAL>
2022-11-07 16:07:03 +0100

    the checkForSameNumbers method was expanded, which checks the entered numbers for their repetitions in the RepeatedNumbers class

--- src/main/java/Lotto/Inspect/RepeatedNumbers.java
+++ src/main/java/Lotto/Inspect/RepeatedNumbers.java
@@ -2,55 +2,55 @@ package Lotto.Inspect;
 
 import Lotto.UserDataInput.NumberFacade;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class RepeatedNumbers {
 
     public static void main(String[] args) {
 
-        int checkForSameNumbers = checkForSameNumbers();
-        System.out.println(checkForSameNumbers);
+        RepeatedNumbers repeatedNumbers = new RepeatedNumbers();
+        repeatedNumbers.checkForSameNumbers();
 
     }
 
-    protected static int checkForSameNumbers (){
+    protected int[] checkForSameNumbers (){
 
         Scanner scanner = new Scanner(System.in);
 
-        Integer [] tabWithInts = new Integer[]{0, 0, 0, 0, 0, 0};
+        int [] enteredIntsArray = new int[6];
 
-        for (int i = 0; i < tabWithInts.length; i++) {
+        HashSet<Integer> setEnteredInts = new HashSet<>();
 
-            System.out.print("\nYour int no. " + (i+1) + ": " );
+        System.out.println("Enter 6 integers from 1 to 49: ");
+
+        for (int i = 0; i < enteredIntsArray.length; i++) {
+
+            System.out.print("\nNumber " + (i+1) + ": ");
             int nextInt = scanner.nextInt();
 
+            enteredIntsArray[i] = nextInt;
 
-            if (tabWithInts[i] != nextInt){
+            setEnteredInts.add(nextInt);
 
-            }else {
+            int setEnteredIntsSize = setEnteredInts.size();
 
-                System.out.println("JEST!");
+            System.out.println("Entered numbers: ");
+            for (int x:enteredIntsArray) {
+                System.out.print(x + "|");
+            }
+            System.out.println();
+
+            if (setEnteredIntsSize != (i+1)){
+
+                System.out.println("\nNumbers cannot repeat themselves! We only list unique numbers! Start again :)");
+                System.exit(0);
 
             }
-
-            tabWithInts[i] = nextInt;
-
-            System.out.println("Your numbers: ");
-
-            for (Integer integer:tabWithInts) {
-                System.out.print(integer + "|");
-
-            }
-
-
 
         }
 
-        /*for (Integer integer:tabWithInts) {
-            System.out.print(integer + "|");
-        }*/
-
-        return 0;
+        return enteredIntsArray;
     }
 
 }
